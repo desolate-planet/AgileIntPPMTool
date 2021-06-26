ac348c6b44c3e5ee3aba87848a40c038a9e53f79
Stephen Munro <stephen.ross.munro@gmail.com>
2021-06-26 18:29:43 +0100

    added findAll method for project retrieval

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/web/ProjectController.java
@@ -41,4 +41,10 @@ public class ProjectController {
         Project project = projectService.findProjectByIdentifier(projectId);
         return new ResponseEntity<Project>(project, HttpStatus.OK);
     }
+
+    @GetMapping("/all")
+    public Iterable<Project> getAllProjects() {
+        return projectService.findAllProjects();
+    }
+
 }
