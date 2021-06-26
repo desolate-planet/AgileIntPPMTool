4da4144d0d8de1a34d8e589ddecbfde9c29b4478
Stephen Munro <stephen.ross.munro@gmail.com>
2021-06-26 15:33:26 +0100

    adding initial controller to save project

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/repositories/ProjectRepository.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/repositories/ProjectRepository.java
@@ -1,4 +1,4 @@
-package io.agileintelligence.ppmtool.repository;
+package io.agileintelligence.ppmtool.repositories;
 
 import io.agileintelligence.ppmtool.domain.Project;
 import org.springframework.data.repository.CrudRepository;
@@ -7,6 +7,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, Long> {
 
-    @Override
-    Iterable<Project> findAllById(Iterable<Long> iterable);
 }
